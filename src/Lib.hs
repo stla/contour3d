e452caff1c8404f2d6ba8e00275c2f2ac12a139d
Stéphane Laurent <stephane.x.laurent@gsk.com>
2018-04-04 14:54:11 +0200

    edges table

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,11 @@ module Lib
 import           Data.Array.Unboxed (UArray, amap, array, bounds, (!))
 -- import           Data.Array.Unboxed (UArray, amap, array, bounds, indices, ixmap, range, (!))
 -- import qualified Data.Array.Unboxed as A
--- import           Data.List
+import           Data.List          (transpose)
 import           Data.Tuple.Extra   (swap)
 import           Matrices
+import           Tables
+import           Utils              (toTriplet)
 
 v' :: UArray (Int,Int,Int) Double
 v' = array ((1,1,1),(3,3,3))
@@ -26,9 +28,9 @@ faceType v nx ny level maxvol = foldr matricialSum v1 [v2,v3,v4]
   v4 = scaledMatrix 8 (minorMatrix v0 nx 1)
 
 levCells :: UArray (Int,Int,Int) Double -> Double -> Double
-         -> ([Int],[Int],[Int],[Int])
+         -> (([Int],[Int],[Int]),[Int])
 levCells a level maxvol = -- (concatMap (fst.f) [1 .. (nz-1)], concatMap (snd.f) [1 .. (nz-1)])
-  (v_i, v_j, v_k, concatMap snd cellsAndTypes)
+  ((v_i, v_j, v_k), concatMap snd cellsAndTypes)
   where
   ((_,_,_),(nx,ny,nz)) = bounds a
   types = map (\k -> faceType (toMatrix a k) nx ny level maxvol) [1 .. nz]
@@ -43,3 +45,29 @@ levCells a level maxvol = -- (concatMap (fst.f) [1 .. (nz-1)], concatMap (snd.f)
   v_i = map ((+1) . (`mod` (nx-1))) cells
   v_j = map ((+1) . (`mod` (ny-1)) . (`div` (nx-1))) cells
   v_k = map ((+1) . (`div` ((nx-1)*(ny-1)))) cells
+
+getBasic :: [Int] -> UArray (Int,Int,Int) Double -> Double -> (([Int],[Int],[Int]),[Int])
+         -> ([[Double]], [Int], [Int])
+getBasic r vol level ((v_i,v_j,v_k),v_t) = (information, p1, cases)
+  where
+  cube_1 = transpose [v_i,v_j,v_k]
+  index :: [[Int]]
+  index = [ [0, 0, 0],
+            [1, 0, 0],
+            [1, 1, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [1, 0, 1],
+            [1, 1, 1],
+            [0, 1, 1] ]
+  k1 = concat $ replicate (length v_i) index
+  k2 = concatMap (replicate 8) cube_1
+  cube_co = zipWith (zipWith (+)) k1 k2
+  value = map (subtract level) [vol ! toTriplet ijk | ijk <- cube_co] ++ [0]
+  information = transpose $ transpose (map (map fromIntegral) (cube_co ++ [[0,0,0]])) ++ [value]
+  -- on verra si c'est bien de concaténer cube_co et value
+  p1 = map ((+1) . (*8)) [0 .. length r -1]
+  cases = [v_t !! (i-1) | i <- r]
+
+test_levCells = levCells v' 22 48
+test_getBasic = getBasic [1,2,3,4,5,6,7] v' 22 test_levCells
--- src/Utils.hs
+++ src/Utils.hs
@@ -3,3 +3,7 @@ module Utils
 
 findIndicesAndItems :: (a -> Bool) -> [a] -> [(Int, a)]
 findIndicesAndItems predicate = filter (predicate . snd) . zip [0..]
+
+toTriplet :: [a] -> (a,a,a)
+toTriplet [x,y,z] = (x,y,z)
+toTriplet _       = undefined
